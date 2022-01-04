0cec222350cb3c0b396dc2953f5a42f74c3d1cbe
Rosie Waite <rosiewaite92@gmail.com>
2022-01-04 17:40:05 +0000

    passed tests for max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,15 @@
 class Oystercard
-  attr_reader:balance
+  attr_reader :balance
+
+MAXIMUM_BALANCE = 90 
 
   def initialize
     @balance = 0
   end
 
-  def add_balance(num)
-    @balance = @balance + num
+  def top_up(num)
+    fail 'You have reached your balance limit' if num + balance > MAXIMUM_BALANCE
+    @balance += num
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,13 +5,21 @@ describe Oystercard do
   it 'balance starts with zero' do
     expect(subject.balance).to eq 0
   end
+
 it 'allow user to add balance' do
   oyster = Oystercard.new
-  expect(oyster).to respond_to(:add_balance).with(1).argument
+  expect(oyster).to respond_to(:top_up).with(1).argument
 end
+
 it 'updates the balance' do
   oyster = Oystercard.new
-  expect(oyster.add_balance(5)).to eq 5
+  expect(oyster.top_up(5)).to eq 5
+end
+
+it 'has a balance limit' do
+  maximum_balance = Oystercard::MAXIMUM_BALANCE
+  subject.top_up(maximum_balance)
+  expect{ subject.top_up 1 }.to raise_error 'You have reached your balance limit'
 end
 
 end
