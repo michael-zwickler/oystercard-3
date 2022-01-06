18f073317829dfcb24d2571a8a3af57803b2a2f7
Adam Hindmarsh <adam_hindmarsh@hotmail.com>
2022-01-06 17:25:11 +0000

    mimimum and penalty fare

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,12 @@
 class Journey 
 
+  PENALTY_FARE = 6
+  MINIMUM_FARE = 1
+
   def initialize()
     @in_journey = false
+    @entry_station = nil
+    @exit_station = nil
   end
 
   def begin_journey(entry_station)
@@ -20,6 +25,15 @@ class Journey
 
   def in_journey?
     return @in_journey
+  end 
+
+  def calculate_fare
+    if @entry_station.nil? && !@exit_station.nil?
+      return PENALTY_FARE
+    elsif !@entry_station.nil? && @exit_station.nil?
+      return PENALTY_FARE
+    end
+    return MINIMUM_FARE
   end 
 
 end 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,5 +2,28 @@ require 'journey'
 
 RSpec.describe Journey do
 
+  describe '#fare' do 
+    describe 'it charges a penalty fare if there is an omission of a exit station' do 
+      it do 
+        subject.begin_journey("Central")
+        expect(subject.calculate_fare).to eq (Journey::PENALTY_FARE)  
+      end
+    end
+
+    describe 'it charges a penalty fare if there is an omission of a entry station' do 
+      it do 
+        subject.end_journey("Central")
+        expect(subject.calculate_fare).to eq (Journey::PENALTY_FARE)  
+      end
+    end
+
+    describe 'it charges a the mininmum fare for a completed journey' do 
+      it do 
+        subject.begin_journey("Central")
+        subject.end_journey("Downtown")
+        expect(subject.calculate_fare).to eq (Journey::MINIMUM_FARE)  
+      end
+    end
+  end
 
 end  
